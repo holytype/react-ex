4429347ff7127851df51a62a6695fa8aa39b4e2a
kss <holytype96@gmail.com>
2024-05-28 12:48:13 +0900

    패스워드 암호화
    spring security - bcryptpassword

--- loginex2/src/main/java/com/example/loginex2/Service/AuthService.java
+++ loginex2/src/main/java/com/example/loginex2/Service/AuthService.java
@@ -7,6 +7,7 @@ import com.example.loginex2.Dto.SignUpDto;
 import com.example.loginex2.Entity.Member;
 import com.example.loginex2.Repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -39,6 +40,18 @@ public class AuthService {
         // UserEntity 생성
         Member member = new Member(dto);
 
+        // 비밀번호 암호화
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(password);
+
+        boolean isPasswordMatch = passwordEncoder.matches(password, hashedPassword);
+
+        if(!isPasswordMatch) {
+            return ResponseDto.setFailed("암호화에 실패하였습니다.");
+        }
+        member.setPassword(hashedPassword);
+
+
         // UserRepository를 이용하여 DB에 Entity 저장 (데이터 적재)
         try {
             memberRepository.save(member);
@@ -52,22 +65,23 @@ public class AuthService {
     public ResponseDto<LoginResponseDto> login(LoginDto dto) {
         String email = dto.getEmail();
         String password = dto.getPassword();
-
-        try {
-            // 사용자 id/password 일치하는지 확인
-            boolean existed = memberRepository.existsByEmailAndPassword(email, password);
-            if(!existed) {
-                return ResponseDto.setFailed("입력하신 로그인 정보가 존재하지 않습니다.");
-            }
-        } catch (Exception e) {
-            return ResponseDto.setFailed("데이터베이스 연결에 실패하였습니다.");
-        }
-
         Member member = null;
 
         try {
-            // 값이 존재하는 경우 사용자 정보 불러옴 (기준 email)
-            member = memberRepository.findById(email).get();
+            // 이메일로 사용자 정보 가져오기
+            member = memberRepository.findById(email).orElse(null);
+            if(member == null) {
+                return ResponseDto.setFailed("입력하신 이메일로 등록된 계정이 존재하지 않습니다.");
+            }
+
+            // 사용자가 입력한 비밀번호를 BCryptPasswordEncoder를 사용하여 암호화
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String encodedPassword = member.getPassword();
+
+            // 저장된 암호화된 비밀번호와 입력된 암호화된 비밀번호 비교
+            if(!passwordEncoder.matches(password, encodedPassword)) {
+                return ResponseDto.setFailed("비밀번호가 일치하지 않습니다.");
+            }
         } catch (Exception e) {
             return ResponseDto.setFailed("데이터베이스 연결에 실패하였습니다.");
         }
